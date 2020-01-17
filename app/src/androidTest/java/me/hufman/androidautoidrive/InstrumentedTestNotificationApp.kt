8141037a3673ab8b9b0bc9c6904bb0224a1721b2
Walter Huf <hufman@gmail.com>
2020-01-16 21:07:38 -0800

    Fixes InstrumentedTestNotificationApp for the new CarNotification

--- app/src/androidTest/java/me/hufman/androidautoidrive/InstrumentedTestNotificationApp.kt
+++ app/src/androidTest/java/me/hufman/androidautoidrive/InstrumentedTestNotificationApp.kt
@@ -53,7 +53,7 @@ class InstrumentedTestNotificationApp {
 		// prepare a notification
 		val icon = Icon.createWithResource(appContext, R.mipmap.ic_launcher)
 		val notification = CarNotification(appContext.packageName, "test", icon, true, arrayOf(),
-				"Test", "Test Summary", "Test Text")
+				"Test", "Test Summary", "Test Text", null, null)
 
 		// send an update from the phone
 		val controller = NotificationListenerServiceImpl.NotificationUpdater(appContext)
@@ -80,14 +80,15 @@ class InstrumentedTestNotificationApp {
 		// prepare a notification
 		val icon = Icon.createWithResource(appContext, R.mipmap.ic_launcher)
 		val notification = CarNotification(appContext.packageName, "test", icon, true, arrayOf(),
-				"Test", "Test Summary", "Test Text")
+				"Test", "Test Summary", "Test Text", null, null)
 
-		// send an interaction from the car
 		val carController = CarNotificationControllerIntent(appContext)
+		// send an interaction from the car
 		carController.clear(notification)
-
-		// verify that it made it across
 		await().untilAsserted { verify(mockListener, times(1)).cancelNotification(notification.key) }
+		// send a custom action from the car
+		carController.action(notification, "custom")
+		await().untilAsserted { verify(mockListener, times(1)).sendNotificationAction(notification.key, "custom") }
 
 	}
 }
