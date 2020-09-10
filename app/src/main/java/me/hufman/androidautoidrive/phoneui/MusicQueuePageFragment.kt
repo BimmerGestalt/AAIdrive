bbf3b8de96cc226b613413701ee87c55eec64e5e
Ben2356 <BenM2356@gmail.com>
2020-09-09 23:57:59 -0500

    android queue view update

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/MusicQueuePageFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/MusicQueuePageFragment.kt
@@ -8,20 +8,17 @@ import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.LruCache
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import com.spotify.protocol.types.ImageUri
 import kotlinx.android.synthetic.main.music_queuepage.*
 import kotlinx.coroutines.*
 import me.hufman.androidautoidrive.R
 import me.hufman.androidautoidrive.music.MusicController
 import me.hufman.androidautoidrive.music.MusicMetadata
-import java.util.concurrent.Semaphore
-import java.util.concurrent.TimeUnit
+import me.hufman.androidautoidrive.music.QueueMetadata
 import kotlin.collections.ArrayList
 import kotlin.coroutines.CoroutineContext
 
@@ -44,7 +41,7 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 
 	var loaderJob: Job? = null
 	val contents = ArrayList<MusicMetadata>()
-	val cachedCoverArtImages = LruCache<String?,Bitmap?>(50)
+	var currentQueueMetadata: QueueMetadata? = null
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.music_queuepage, container, false)
@@ -58,7 +55,7 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 
 		listQueue.setHasFixedSize(true)
 		listQueue.layoutManager = LinearLayoutManager(this.context)
-		listQueue.adapter = QueueAdapter(this.context!!, contents, cachedCoverArtImages) { mediaEntry ->
+		listQueue.adapter = QueueAdapter(this.context!!, contents) { mediaEntry ->
 			if(mediaEntry != null) {
 				musicController.playQueue(mediaEntry)
 				(activity as MusicPlayerActivity).showNowPlaying()
@@ -66,26 +63,31 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 		}
 
 		listQueueRefresh.setOnRefreshListener {
-			queueDirectory()
+			redrawQueueUI()
 			Handler(this.context?.mainLooper).postDelayed({
 				listQueueRefresh.isRefreshing = false
 			}, 1000)
 		}
 
-		queueDirectory()
-	}
-
-	fun queueDirectory() {
 		txtQueueEmpty.text = getString(R.string.MUSIC_BROWSE_LOADING)
 
+		musicController.listener = Runnable {
+			if(currentQueueMetadata?.title != musicController.getQueue()?.title || currentQueueMetadata?.songs?.size != musicController.getQueue()?.songs?.size) {
+				redrawQueueUI()
+			}
+
+			(listQueue.adapter as QueueAdapter).notifyDataSetChanged()
+		}
+	}
+
+	fun redrawQueueUI() {
 		if(loaderJob != null) {
 			loaderJob?.cancel()
 		}
 
-		//reason why app doesn't start with the queue already loaded is due to async nature of getting the queue and it not being ready when getQueue is called
-		//need to have a redraw method or something that periodically checks to see if the musicController.getQueue is not empty and if it is then display everything otherwise keep waiting
 		loaderJob = launch {
-			val result = musicController.getQueue()?.songs
+			currentQueueMetadata = musicController.getQueue()
+			val result = currentQueueMetadata?.songs
 			this@MusicQueuePageFragment.contents.clear()
 			result?.forEach {
 				this@MusicQueuePageFragment.contents.add(it)
@@ -102,7 +104,7 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 				listQueue.adapter?.notifyDataSetChanged()
 			}
 
-			val coverArtImage = musicController.getQueue()?.coverArt
+			val coverArtImage = currentQueueMetadata?.coverArt
 			if(coverArtImage != null) {
 				playlistCoverArt.setImageBitmap(coverArtImage)
 			}
@@ -110,12 +112,12 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 				playlistCoverArt.setImageBitmap(placeholderCoverArt)
 			}
 
-			playlistName.setText(musicController.getQueue()?.title)
+			playlistName.setText(currentQueueMetadata?.title)
 		}
 	}
 
-	inner class QueueAdapter(val context: Context, val contents: ArrayList<MusicMetadata>, val cachedCoverArtImages: LruCache<String?,Bitmap?>, val clickListener: (MusicMetadata?) -> Unit): RecyclerView.Adapter<QueueAdapter.ViewHolder>() {
-		inner class ViewHolder(val view: View, val coverArtImageView: ImageView): RecyclerView.ViewHolder(view), View.OnClickListener {
+	inner class QueueAdapter(val context: Context, val contents: ArrayList<MusicMetadata>, val clickListener: (MusicMetadata?) -> Unit): RecyclerView.Adapter<QueueAdapter.ViewHolder>() {
+		inner class ViewHolder(val view: View, val checkmarkImageView: ImageView, val coverArtImageView: ImageView, val songTextView: TextView, val artistTextView: TextView): RecyclerView.ViewHolder(view), View.OnClickListener {
 			init {
 				view.setOnClickListener(this)
 			}
@@ -131,65 +133,21 @@ class MusicQueuePageFragment: Fragment(), CoroutineScope {
 		}
 
 		override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-			//TODO change to own listitem
-			val layout = LayoutInflater.from(context).inflate(R.layout.music_browse_listitem, parent, false)
-			return ViewHolder(layout, layout.findViewById(R.id.imgBrowseType))
+			val layout = LayoutInflater.from(context).inflate(R.layout.music_queue_listitem, parent, false)
+			return ViewHolder(layout, layout.findViewById(R.id.checkmarkImage), layout.findViewById(R.id.coverArtImage), layout.findViewById(R.id.songText), layout.findViewById(R.id.artistText))
 		}
-
-//		inner class Test {
-//			private val semaphore = Semaphore(1)
-//			var counter = 0
-//				private set
-//
-//			fun retrieveCoverArt(coverArtUri: String, mediaId: String, holder: ViewHolder) {
-//				semaphore.acquire()
-//				counter++
-//				launch {
-//					val coverArt = musicController.getSongCoverArtAsync(ImageUri(coverArtUri)).await()
-//					cachedCoverArtImages.put(mediaId, coverArt)
-//					holder.coverArtImageView.setImageBitmap(coverArt)
-//					//delay(TimeUnit.SECONDS.toSeconds(10))
-//					semaphore.release()
-//				}
-//			}
-//
-//			fun t() {
-//				s.acquire()
-//				launch {
-//					delay(TimeUnit.SECONDS.toSeconds(5))
-//					s.release()
-//				}
-//			}
-//		}
-//
-//		val test = Test()
-//
-//		val s = Semaphore(1)
 
 		override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 			val item = contents.getOrNull(position) ?: return
 
-//			val cachedCoverArtImage = cachedCoverArtImages[item.mediaId]
-//			holder.coverArtImageView.setImageBitmap(cachedCoverArtImage)
-//
-//			if(cachedCoverArtImage == null) {
-//				launch {
-//					val coverArt = musicController.getSongCoverArtAsync(ImageUri(item.coverArtUri)).await()
-//					cachedCoverArtImages.put(item.mediaId, coverArt)
-//					holder.coverArtImageView.setImageBitmap(coverArt)
-//				}
+			if(item.queueId == musicController.getMetadata()?.queueId) {
+				holder.checkmarkImageView.setImageDrawable(resources.getDrawable(R.drawable.ic_equalizer_black_24dp, null))
+			} else {
+				holder.checkmarkImageView.setImageDrawable(null)
+			}
 			holder.coverArtImageView.setImageBitmap(item.coverArt)
-
-				//test.retrieveCoverArt(item.coverArtUri!!, item.mediaId!!, holder)
-
-//				launch {
-//					//test.t()
-//					//delay(15000)
-//				}
-//			}
-
-			holder.view.findViewById<TextView>(R.id.txtBrowseEntryTitle).setText(item.title)
-			holder.view.findViewById<TextView>(R.id.txtBrowseEntrySubtitle).setText(item.artist)
+			holder.songTextView.setText(item.title)
+			holder.artistTextView.setText(item.artist)
 		}
 	}
 }
