ca8dcd3957b68e5825cdd3b44f99769d5a7bd913
Walter Huf <hufman@gmail.com>
2020-10-23 22:27:11 -0700

    Splits out AM app management to a separate helper object
    Fixes #132

--- app/src/main/java/me/hufman/androidautoidrive/music/MusicAppInfo.kt
+++ app/src/main/java/me/hufman/androidautoidrive/music/MusicAppInfo.kt
@@ -2,15 +2,19 @@ package me.hufman.androidautoidrive.music
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import me.hufman.androidautoidrive.carapp.AMAppInfo
+import me.hufman.androidautoidrive.carapp.AMCategory
 
-data class MusicAppInfo(val name: String, val icon: Drawable,
-                        val packageName: String, val className: String?) {
+data class MusicAppInfo(override val name: String, override val icon: Drawable,
+                        override val packageName: String, val className: String?): AMAppInfo {
 	var probed = false
 	var connectable = false     // whether MediaBrowser can connect
 	var controllable = false    // whether MediaSession can control it
 	var browseable = false      // whether any media items were discovered
 	var searchable = false      // whether any search results came back
 	var playsearchable = false  // whether the controller indicated PlayFromSearch support
+
+	override val category = AMCategory.MULTIMEDIA
 
 	companion object {
 		fun getInstance(context: Context, packageName: String, className: String?): MusicAppInfo {
