a422705762a11182071be9635984826ee3b88b76
Walter Huf <hufman@gmail.com>
2020-12-08 19:03:11 -0800

    Adds a Github Action to build the project

---
+++ .github/actions/download-externals-action/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/BMW_Connected_Classic_v1.8_(usa_160214_1142)_apkpure.com.apk'
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/MINI_Connected_Classic_v1.1.3_(usa_160214_448)_apkpure.com.apk'
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/BMW_Connected_v3.1.1.3078_apkpure.com.apk'
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/MINI_Connected_v3.1.1.3078_apkpure.com.apk'
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/SmartThings_Classic_v2.1.6_apkpure.com.apk'
+wget --quiet -P external 'https://androidautoidrive.s3.amazonaws.com/external/Spotify_Music_v8.4.32.623_apkpure.com.apk'
