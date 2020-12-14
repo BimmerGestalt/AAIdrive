d5a63ded557163b7ca150d9d2e0ae70ad2e84d49
Walter Huf <hufman@gmail.com>
2020-12-13 16:24:24 -0800

    Adds a LiveData way to access AppSettings

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/SetupActivity.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/SetupActivity.kt
@@ -1,35 +1,13 @@
 package me.hufman.androidautoidrive.phoneui
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import kotlinx.android.synthetic.main.activity_setup.*
 import me.hufman.androidautoidrive.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SetupActivity : AppCompatActivity() {
-	companion object {
-		const val REDRAW_DEBOUNCE = 100
-		const val INTENT_REDRAW = "me.hufman.androidautoidrive.SetupActivity.REDRAW"
-		fun redraw(context: Context) {
-			context.sendBroadcast(Intent(INTENT_REDRAW))
-		}
-	}
-
-	val appSettings by lazy { MutableAppSettingsReceiver(this) }
-
-	val redrawListener = object: BroadcastReceiver() {
-		override fun onReceive(p0: Context?, p1: Intent?) {
-			redraw()
-		}
-	}
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
@@ -38,39 +16,13 @@ class SetupActivity : AppCompatActivity() {
 		val buildTime = SimpleDateFormat.getDateTimeInstance().format(Date(BuildConfig.BUILD_TIME))
 		txtBuildInfo.text = getString(R.string.txt_build_info, BuildConfig.VERSION_NAME, buildTime)
 
-		swAdvancedSettings.setOnClickListener {
-			appSettings[AppSettings.KEYS.SHOW_ADVANCED_SETTINGS] = swAdvancedSettings.isChecked.toString()
-			redraw()
+		val advancedSetting = BooleanLiveSetting(this, AppSettings.KEYS.SHOW_ADVANCED_SETTINGS)
+		advancedSetting.observe(this, androidx.lifecycle.Observer {
+			swAdvancedSettings.isChecked = it
+			paneAdvancedInfo.visible = it
+		})
+		swAdvancedSettings.setOnCheckedChangeListener { _, isChecked ->
+			advancedSetting.setValue(isChecked)
 		}
-
-		this.registerReceiver(redrawListener, IntentFilter(INTENT_REDRAW))
-	}
-
-	override fun onResume() {
-		super.onResume()
-
-		redraw()
-	}
-
-	override fun onDestroy() {
-		super.onDestroy()
-		this.unregisterReceiver(redrawListener)
-	}
-
-	fun redraw() {
-		if (!lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-			return
-		}
-
-		// only redraw on the main thread
-		if (!Looper.getMainLooper().isCurrentThread) {
-			sendBroadcast(Intent(INTENT_REDRAW))
-			return
-		}
-
-		// toggle the advanced details
-		val showAdvancedSettings = appSettings[AppSettings.KEYS.SHOW_ADVANCED_SETTINGS].toBoolean()
-		swAdvancedSettings.isChecked = showAdvancedSettings
-		paneAdvancedInfo.visible = showAdvancedSettings
 	}
 }
--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MusicAdvancedSettingsFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MusicAdvancedSettingsFragment.kt
@@ -5,14 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_music_advancedsettings.*
 import me.hufman.androidautoidrive.AppSettings
-import me.hufman.androidautoidrive.MutableAppSettingsReceiver
+import me.hufman.androidautoidrive.BooleanLiveSetting
 import me.hufman.androidautoidrive.R
 
 class MusicAdvancedSettingsFragment: Fragment() {
-	val appSettings by lazy { MutableAppSettingsReceiver(requireContext()) }
-
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.fragment_music_advancedsettings, container, false)
 	}
@@ -20,17 +19,20 @@ class MusicAdvancedSettingsFragment: Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		val audioContextSetting = BooleanLiveSetting(requireContext(), AppSettings.KEYS.AUDIO_FORCE_CONTEXT)
+		audioContextSetting.observe(this, Observer {
+			swAudioContext.isChecked = it
+		})
 		swAudioContext.setOnCheckedChangeListener { _, isChecked ->
-			appSettings[AppSettings.KEYS.AUDIO_FORCE_CONTEXT] = isChecked.toString()
+			audioContextSetting.setValue(isChecked)
 		}
-		swSpotifyLayout.setOnCheckedChangeListener { _, isChecked ->
-			appSettings[AppSettings.KEYS.FORCE_SPOTIFY_LAYOUT] = isChecked.toString()
-		}
-		redraw()
-	}
 
-	fun redraw() {
-		swAudioContext.isChecked = appSettings[AppSettings.KEYS.AUDIO_FORCE_CONTEXT].toBoolean()
-		swSpotifyLayout.isChecked = appSettings[AppSettings.KEYS.FORCE_SPOTIFY_LAYOUT].toBoolean()
+		val spotifyLayoutSetting = BooleanLiveSetting(requireContext(), AppSettings.KEYS.FORCE_SPOTIFY_LAYOUT)
+		spotifyLayoutSetting.observe(this, Observer {
+			swSpotifyLayout.isChecked = it
+		})
+		swSpotifyLayout.setOnCheckedChangeListener { _, isChecked ->
+			spotifyLayoutSetting.setValue(isChecked)
+		}
 	}
 }
