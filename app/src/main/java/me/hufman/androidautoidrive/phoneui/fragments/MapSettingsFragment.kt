7379ded11258c98668fe1e3cb7e61b717ab8187b
Walter Huf <hufman@gmail.com>
2020-12-19 11:57:37 -0800

    Converts MapSettings to Data Binding

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MapSettingsFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MapSettingsFragment.kt
@@ -4,51 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_map_settings.*
-import me.hufman.androidautoidrive.*
-import java.util.*
-import kotlin.math.max
+import androidx.lifecycle.ViewModelProvider
+import me.hufman.androidautoidrive.databinding.MapSettingsBinding
+import me.hufman.androidautoidrive.phoneui.viewmodels.MapSettingsModel
 
 class MapSettingsFragment: Fragment() {
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		return inflater.inflate(R.layout.fragment_map_settings, container, false)
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		val styleSetting = StringLiveSetting(requireContext(), AppSettings.KEYS.GMAPS_STYLE)
-		styleSetting.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-			val mapStylePosition = resources.getStringArray(R.array.gmaps_styles).map { title ->
-				title.toLowerCase(Locale.ROOT).replace(' ', '_')
-			}.indexOf(it.toLowerCase(Locale.ROOT))
-			swGmapSyle.setSelection(max(0, mapStylePosition))
-		})
-		swGmapSyle.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-			override fun onNothingSelected(parent: AdapterView<*>?) {
-			}
-			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				val value = parent?.getItemAtPosition(position) ?: return
-				styleSetting.setValue(value.toString().toLowerCase(Locale.ROOT).replace(' ', '_'))
-			}
-		}
-
-		val widescreenSetting = BooleanLiveSetting(requireContext(), AppSettings.KEYS.MAP_WIDESCREEN)
-		widescreenSetting.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-			swMapWidescreen.isChecked = it
-		})
-		swMapWidescreen.setOnCheckedChangeListener { _, isChecked ->
-			widescreenSetting.setValue(isChecked)
-		}
-
-		val invertZoomSetting = BooleanLiveSetting(requireContext(), AppSettings.KEYS.MAP_INVERT_SCROLL)
-		invertZoomSetting.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-			swMapInvertZoom.isChecked = it
-		})
-		swMapInvertZoom.setOnCheckedChangeListener { _, isChecked ->
-			widescreenSetting.setValue(isChecked)
-		}
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+		val settingsModel = ViewModelProvider(this, MapSettingsModel.Factory(requireContext().applicationContext)).get(MapSettingsModel::class.java)
+		val binding = MapSettingsBinding.inflate(inflater, container, false)
+		binding.lifecycleOwner = viewLifecycleOwner
+		binding.settings = settingsModel
+		return binding.root
 	}
 }
--- app/src/main/java/me/hufman/androidautoidrive/phoneui/viewmodels/BindingAdapters.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/viewmodels/BindingAdapters.kt
@@ -2,9 +2,12 @@ package me.hufman.androidautoidrive.phoneui.viewmodels
 
 import android.graphics.Bitmap
 import android.view.View
-import android.widget.ImageView
+import android.widget.*
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import me.hufman.androidautoidrive.phoneui.visible
+import kotlin.math.max
 
 
 @BindingAdapter("android:src")
@@ -19,4 +22,44 @@ fun setImageViewResource(view: ImageView, resource: Int) {
 @BindingAdapter("android:visibility")
 fun setViewVisibility(view: View, visible: Boolean) {
 	view.visible = visible
+}
+
+/**
+ * Finds the index of the given item in an Adapter
+ * Which aren't normally iterable, so now they are
+ * May return -1 if the item doesn't seem to exist
+ */
+fun <T> Adapter.indexOf(item: T): Int {
+	for (i in 0 until count) {
+		if (getItem(i) == item) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Set up the spinner based on the selectedValue from the LiveData
+@BindingAdapter("selectedValue")
+fun setSelectedValue(spinner: Spinner, selectedValue: String) {
+	val position = spinner.adapter.indexOf(selectedValue)
+	spinner.setSelection(max(0, position))
+}
+// Set up the spinner's event listeners
+@BindingAdapter("selectedValueAttrChanged")
+fun setInverseBindingListener(spinner: Spinner, inverseBindingListener: InverseBindingListener?) {
+	if (inverseBindingListener == null) {
+		spinner.onItemSelectedListener = null
+	} else {
+		spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+				inverseBindingListener.onChange()
+			}
+			override fun onNothingSelected(parent: AdapterView<*>?) {}
+		}
+	}
+}
+// Triggered when the event listener fires from above
+@InverseBindingAdapter(attribute="selectedValue", event="selectedValueAttrChanged")
+fun getSelectedValue(spinner: Spinner): String {
+	return spinner.selectedItem.toString()
 }
