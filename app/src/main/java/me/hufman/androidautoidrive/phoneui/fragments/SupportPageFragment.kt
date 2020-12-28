e9cb59cfaba0eddc2e99e50ef998e96f3f994ccc
Walter Huf <hufman@gmail.com>
2020-12-27 18:22:31 -0800

    Adds project support info
    Fixes #129

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/SupportPageFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/SupportPageFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_supportpage.*
 import me.hufman.androidautoidrive.BuildConfig
 import me.hufman.androidautoidrive.R
+import me.hufman.androidautoidrive.phoneui.DonationRequest
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,9 +22,33 @@ class SupportPageFragment: Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		btn_github_discussions.setOnClickListener {
+		btn_donations.setOnClickListener {
+			val intent = Intent(Intent.ACTION_VIEW).apply {
+				data = Uri.parse(DonationRequest.DONATION_URL)
+				flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			}
+			requireContext().startActivity(intent)
+		}
+
+		btn_support_feedback.setOnClickListener {
 			val intent = Intent(Intent.ACTION_VIEW).apply {
 				data = Uri.parse("https://github.com/hufman/AndroidAutoIdrive/discussions")
+				flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			}
+			requireContext().startActivity(intent)
+		}
+
+		btn_support_issues.setOnClickListener {
+			val intent = Intent(Intent.ACTION_VIEW).apply {
+				data = Uri.parse("https://github.com/hufman/AndroidAutoIdrive/issues")
+				flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			}
+			requireContext().startActivity(intent)
+		}
+
+		btn_support_share.setOnClickListener {
+			val intent = Intent(Intent.ACTION_VIEW).apply {
+				data = Uri.parse("https://github.com/hufman/AndroidAutoIdrive")
 				flags = Intent.FLAG_ACTIVITY_NEW_TASK
 			}
 			requireContext().startActivity(intent)
