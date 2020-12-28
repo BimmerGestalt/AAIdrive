3f928874ac74b95eef6d3118069328448ea0431e
Walter Huf <hufman@gmail.com>
2020-12-27 19:56:18 -0800

    Factors out the Music Permissions Pane

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MusicPageFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MusicPageFragment.kt
@@ -5,34 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_musicpage.*
 import me.hufman.androidautoidrive.*
-import me.hufman.androidautoidrive.phoneui.controllers.PermissionsController
-import me.hufman.androidautoidrive.phoneui.viewmodels.PermissionsModel
-import me.hufman.androidautoidrive.phoneui.visible
 
 class MusicPageFragment: Fragment() {
-	val permissionsController by lazy { PermissionsController(requireActivity()) }
-	val viewModel by lazy { PermissionsModel.Factory(requireContext().applicationContext).create(PermissionsModel::class.java) }
-
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.fragment_musicpage, container, false)
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		btnGrantSessions.setOnClickListener {
-			permissionsController.promptNotification()
-		}
-
-		viewModel.hasNotificationPermission.observe(viewLifecycleOwner) {
-			paneGrantSessions.visible = !it
-		}
-	}
-
-	override fun onResume() {
-		super.onResume()
-		viewModel.update()
 	}
 }
