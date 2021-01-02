cbffbd40f865563c1e9cf9ee8b64563a561c7b97
Walter Huf <hufman@gmail.com>
2021-01-01 16:55:23 -0800

    Shows a summarized view of dependencies on Overview page

---
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/DependencySummaryFragment.kt
@@ -0,0 +1,23 @@
+package me.hufman.androidautoidrive.phoneui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import me.hufman.androidautoidrive.databinding.DependencySummaryBinding
+import me.hufman.androidautoidrive.phoneui.controllers.DependencyInfoController
+import me.hufman.androidautoidrive.phoneui.viewmodels.DependencyInfoModel
+
+class DependencySummaryFragment: Fragment() {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		val controller = DependencyInfoController(requireContext().applicationContext)
+		val viewModel = ViewModelProvider(this, DependencyInfoModel.Factory(requireContext().applicationContext)).get(DependencyInfoModel::class.java)
+		val binding = DependencySummaryBinding.inflate(inflater, container, false)
+		binding.lifecycleOwner = viewLifecycleOwner
+		binding.controller = controller
+		binding.viewModel = viewModel
+		return binding.root
+	}
+}
