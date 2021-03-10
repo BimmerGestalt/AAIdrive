ad99ba9b6d56992bcfb46575b7cb29d5817d7684
Walter Huf <hufman@gmail.com>
2021-03-09 20:44:10 -0800

    Recreates the Notification app after leaving it
    Recreating the app while handling the Notification Center click
    doesn't show a loading spinner like the AM App,
    so try to recreate opportunistically after we use a Focus State Event
    Fixes #122 for Notification

--- app/src/main/java/me/hufman/androidautoidrive/carapp/notifications/PhoneNotifications.kt
+++ app/src/main/java/me/hufman/androidautoidrive/carapp/notifications/PhoneNotifications.kt
@@ -45,6 +45,7 @@ class PhoneNotifications(val iDriveConnectionStatus: IDriveConnectionStatus, val
 	val carApp: RHMIApplicationSynchronized
 	val amHandle: Int
 	val focusTriggerController: FocusTriggerController
+	val focusedStateTracker = FocusedStateTracker()
 	val showNotificationController: ShowNotificationController
 	val readHistory = PopupHistory()       // suppress any duplicate New Notification actions
 	val viewPopup: PopupView                // notification about notification
@@ -144,6 +145,26 @@ class PhoneNotifications(val iDriveConnectionStatus: IDriveConnectionStatus, val
 		}
 	}
 
+	/**
+	 * Check if we should recreate the app
+	 * Is called from within an HMI focused event handler,
+	 * so sleeping here is inside a background thread
+	 * */
+	fun checkRecreate() {
+		val interval = 500
+		val waitDelay = 10000
+		if (focusTriggerController.hasFocusedState && focusedStateTracker.getFocused() == null) {
+			for (i in 0..waitDelay step interval) {
+				Thread.sleep(500)
+				if (focusedStateTracker.getFocused() != null) {
+					return
+				}
+			}
+			// waited the entire time without getting focused, recreate
+			recreateRhmiApp()
+		}
+	}
+
 	/** creates the app in the car */
 	fun createRhmiApp(): RHMIApplication {
 		// load the resources
@@ -169,6 +190,8 @@ class PhoneNotifications(val iDriveConnectionStatus: IDriveConnectionStatus, val
 			carConnection.rhmi_dispose(rhmiHandle)
 			// create a new one
 			carAppSwappable.app = createRhmiApp()
+			// clear FocusTriggerController because of the new rhmi app
+			focusTriggerController.hasFocusedState = false
 			// reconnect, triggering a sync down to the new RHMI Etch app
 			carAppSwappable.isConnected = true
 		}
@@ -242,6 +265,12 @@ class PhoneNotifications(val iDriveConnectionStatus: IDriveConnectionStatus, val
 
 			val state = app?.states?.get(componentId)
 			state?.onHmiEvent(eventId, args)
+
+			if (state != null && eventId == 1) {
+				val focused = args?.get(4.toByte()) as? Boolean ?: false
+				focusedStateTracker.onFocus(state.id, focused)
+				checkRecreate()
+			}
 
 			val component = app?.components?.get(componentId)
 			component?.onHmiEvent(eventId, args)
