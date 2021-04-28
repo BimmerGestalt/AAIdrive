5a534784a36e540252ed7660ada7904d9b0d995a
Ben2356 <BenM2356@gmail.com>
2021-04-27 22:26:51 -0500

    removed PLAYLIST_MODIFY_PUBLIC scope as it is no longer needed

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/SpotifyAuthorizationActivity.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/SpotifyAuthorizationActivity.kt
@@ -61,7 +61,6 @@ class SpotifyAuthorizationActivity: Activity() {
 	private val scopes = listOf(
 			SpotifyScope.USER_MODIFY_PLAYBACK_STATE.uri,
 			SpotifyScope.USER_LIBRARY_READ.uri,
-			SpotifyScope.PLAYLIST_MODIFY_PUBLIC.uri,
 			SpotifyScope.PLAYLIST_MODIFY_PRIVATE.uri)
 	private val lazyAuthService = lazy {
 		createAuthorizationService()
