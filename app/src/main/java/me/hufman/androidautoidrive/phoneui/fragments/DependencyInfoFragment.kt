412ef33549618416a7581279e475fee551e0d58f
Walter Huf <hufman@gmail.com>
2021-07-02 22:28:35 -0700

    Adds a warning about missing background permission

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/DependencyInfoFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/DependencyInfoFragment.kt
@@ -5,19 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
 import me.hufman.androidautoidrive.databinding.DependencyInfoBinding
 import me.hufman.androidautoidrive.phoneui.controllers.DependencyInfoController
+import me.hufman.androidautoidrive.phoneui.controllers.PermissionsController
 import me.hufman.androidautoidrive.phoneui.viewmodels.DependencyInfoModel
+import me.hufman.androidautoidrive.phoneui.viewmodels.PermissionsModel
 
 class DependencyInfoFragment: Fragment() {
+	val viewModel by activityViewModels<DependencyInfoModel> { DependencyInfoModel.Factory(requireContext().applicationContext) }
+	val permissionsModel by activityViewModels<PermissionsModel> { PermissionsModel.Factory(requireContext().applicationContext) }
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		val controller = DependencyInfoController(requireContext().applicationContext)
-		val viewModel = ViewModelProvider(this, DependencyInfoModel.Factory(requireContext().applicationContext)).get(DependencyInfoModel::class.java)
+		val permissionsController = PermissionsController(requireActivity())
 		val binding = DependencyInfoBinding.inflate(inflater, container, false)
 		binding.lifecycleOwner = viewLifecycleOwner
 		binding.controller = controller
 		binding.viewModel = viewModel
+		binding.permissionsModel = permissionsModel
+		binding.permissionsController = permissionsController
 		return binding.root
+	}
+
+	override fun onResume() {
+		super.onResume()
+		permissionsModel.update()
 	}
 }
