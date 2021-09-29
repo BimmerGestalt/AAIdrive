34191c0a97f9bccff91ac386bc31b383414825f2
Walter Huf <hufman@gmail.com>
2021-09-28 19:12:00 -0700

    Adds a button to info about Addons

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/AddonsPageFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/AddonsPageFragment.kt
@@ -1,9 +1,12 @@
 package me.hufman.androidautoidrive.phoneui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import me.hufman.androidautoidrive.R
 
@@ -12,4 +15,14 @@ class AddonsPageFragment: Fragment() {
 		return inflater.inflate(R.layout.fragment_addonpage, container, false)
 	}
 
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		view.findViewById<Button>(R.id.btnLearn).setOnClickListener {
+			val intent = Intent(Intent.ACTION_VIEW).apply {
+				data = Uri.parse("https://github.com/BimmerGestalt/IDriveConnectAddons")
+				flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			}
+			startActivity(intent)
+		}
+	}
 }
