37b7afbfa255ddd92a1d34bcd2dbf0bfd4e3acc2
Walter Huf <hufman@gmail.com>
2021-10-17 19:35:52 -0700

    Provides Car Capabilities through the Content Provider

--- app/src/main/java/me/hufman/androidautoidrive/CDSContentProvider.kt
+++ app/src/main/java/me/hufman/androidautoidrive/CDSContentProvider.kt
@@ -4,6 +4,7 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.database.AbstractCursor
 import android.database.Cursor
+import android.database.MatrixCursor
 import android.net.Uri
 import android.provider.BaseColumns
 import com.google.gson.JsonObject
@@ -16,6 +17,7 @@ import java.util.*
 class CDSContentProvider: ContentProvider(), CDSEventHandler {
 	companion object {
 		private const val TAG = "CDSContentProvider"
+		private val QUERY_CAPABILITIES_ID = Regex("/capabilities/([a-z.]+)")
 		private val QUERY_CDS_ID = Regex("/cds/(\\d+)")
 
 		// how long to wait on first subscription before returning NULL
@@ -24,8 +26,23 @@ class CDSContentProvider: ContentProvider(), CDSEventHandler {
 		private const val IDLE_UNSUBSCRIBE_TIMEOUT = 10000
 
 		const val CONTENT_PROVIDER_AUTHORITY = "io.bimmergestalt.cardata.provider"
+		const val CONTENT_PROVIDER_CAPABILITIES = "content://$CONTENT_PROVIDER_AUTHORITY/capabilities"
 		const val CONTENT_PROVIDER_CDS = "content://$CONTENT_PROVIDER_AUTHORITY/cds"
 
+		val publicCapabilties = setOf(
+				"a4axl",
+				"alignment_right",
+				"hmi.display-height",
+				"hmi.display-width",
+				"hmi.role",
+				"hmi.type",
+				"map",
+				"navi",
+				"speech2text",
+				"speedlock",
+				"tts",
+				"voice",
+		)
 		val privateCdsProperties = setOf(
 				CDSProperty.COMMUNICATION_CURRENTCALLINFO,
 				CDSProperty.COMMUNICATION_LASTCALLINFO,
@@ -40,6 +57,7 @@ class CDSContentProvider: ContentProvider(), CDSEventHandler {
 				CDSProperty.VEHICLE_VIN,
 		)
 	}
+	private var carInformation = CarInformation()
 	private val cdsData = CDSDataProvider()
 
 	private val latestQuery: MutableMap<CDSProperty, Long> = EnumMap(CDSProperty::class.java)
@@ -52,15 +70,33 @@ class CDSContentProvider: ContentProvider(), CDSEventHandler {
 
 	override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
 		val path = uri.path ?: ""
-		if (!path.startsWith("/cds")) {
+		if (path.startsWith("/capabilities")) {
+			val nameMatch = QUERY_CAPABILITIES_ID.matchEntire(path)
+			return queryCapabilities(nameMatch?.groups?.get(1)?.value)
+		} else if (path.startsWith("/cds")) {
+			val idMatch = QUERY_CDS_ID.matchEntire(path)
+			val property = CDSProperty.fromIdent(idMatch?.groups?.get(1)?.value ?: "")
+			if (property != null) {
+				return queryCds(property)
+			}
 			throw IllegalArgumentException()
 		}
-		val idMatch = QUERY_CDS_ID.matchEntire(uri.path ?: "")
-		val property = CDSProperty.fromIdent(idMatch?.groups?.get(1)?.value ?: "")
-		if (property != null) {
-			return queryCds(property)
-		}
 		return null
+	}
+
+	private fun queryCapabilities(name: String?): Cursor {
+		if (name != null && !publicCapabilties.contains(name)) {
+			throw IllegalArgumentException()
+		}
+		val results = MatrixCursor(arrayOf("name", "value"))
+		carInformation.capabilities.filter {
+			publicCapabilties.contains(it.key) &&
+				(name == null || name == it.key)
+		}.forEach {
+			results.addRow(arrayOf(it.key, it.value))
+		}
+		results.moveToFirst()
+		return results
 	}
 
 	private fun queryCds(property: CDSProperty): Cursor {
@@ -76,8 +112,10 @@ class CDSContentProvider: ContentProvider(), CDSEventHandler {
 	}
 
 	override fun getType(uri: Uri): String? {
-		val path = uri.path ?: ""
+		val path = (uri.path ?: "").trimEnd('/')
 		return when {
+			path == "/capabilities" -> { "vnd.android.cursor.dir/vnd.io.bimmergestalt.cardata.provider.capabilities" }
+			path.startsWith("/capabilities/") ->  { "vnd.android.cursor.item/vnd.io.bimmergestalt.cardata.provider.capabilities" }
 			path == "/cds" -> { "vnd.android.cursor.dir/vnd.io.bimmergestalt.cardata.provider.cds" }
 			path.startsWith("/cds/") -> { "vnd.android.cursor.item/vnd.io.bimmergestalt.cardata.provider.cds" }
 			else -> { null }
