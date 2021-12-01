d4ab5056e0cefc852df959374593d44610777425
Walter Huf <hufman@gmail.com>
2021-12-01 12:44:15 -0800

    Adds information about using Google Calendar

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/viewmodels/CalendarSettingsModel.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/viewmodels/CalendarSettingsModel.kt
@@ -1,10 +1,12 @@
 package me.hufman.androidautoidrive.phoneui.viewmodels
 
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import me.hufman.androidautoidrive.AppSettings
 import me.hufman.androidautoidrive.BooleanLiveSetting
+import me.hufman.androidautoidrive.phoneui.FunctionalLiveData
 
 class CalendarSettingsModel(appContext: Context): ViewModel() {
 	class Factory(val appContext: Context): ViewModelProvider.Factory {
@@ -15,4 +17,11 @@ class CalendarSettingsModel(appContext: Context): ViewModel() {
 	}
 
 	val calendarEnabled = BooleanLiveSetting(appContext, AppSettings.KEYS.ENABLED_CALENDAR)
+
+	val xiaomiCalendarInstalled = FunctionalLiveData<Boolean> {
+		try {
+			appContext.packageManager.getPackageInfo("com.xiaomi.calendar", 0)
+			true
+		} catch (e: PackageManager.NameNotFoundException) { false }
+	}
 }
