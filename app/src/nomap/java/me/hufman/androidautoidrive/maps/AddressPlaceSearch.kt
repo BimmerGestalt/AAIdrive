5ddeb0592dfd406c3d45a95efbb397f7c6e8f56f
Walter Huf <hufman@gmail.com>
2022-01-25 17:11:17 -0800

    Adds a distance value for address searching

--- app/src/nomap/java/me/hufman/androidautoidrive/maps/AddressPlaceSearch.kt
+++ app/src/nomap/java/me/hufman/androidautoidrive/maps/AddressPlaceSearch.kt
@@ -5,6 +5,7 @@ import android.location.Address
 import android.location.Geocoder
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
+import me.hufman.androidautoidrive.CarInformation
 import java.io.IOException
 
 fun Address.addressLines(): List<String> {
@@ -12,21 +13,25 @@ fun Address.addressLines(): List<String> {
 		this.getAddressLine(i)
 	}
 }
-class AddressPlaceSearch(val geocoder: Geocoder): MapPlaceSearch {
+class AddressPlaceSearch(val geocoder: Geocoder, private val locationProvider: CarLocationProvider): MapPlaceSearch {
 	companion object {
 		fun getInstance(context: Context): AddressPlaceSearch {
-			return AddressPlaceSearch(Geocoder(context))
+			return AddressPlaceSearch(Geocoder(context), CdsLocationProvider(CarInformation.cachedCdsData))
 		}
 	}
 
 	override fun searchLocationsAsync(query: String): Deferred<List<MapResult>> {
 		val results = CompletableDeferred<List<MapResult>>()
 		try {
+			val currentLocation = locationProvider.currentLocation?.let {
+				LatLong(it.latitude, it.longitude)
+			}
 			val searchResults = geocoder.getFromLocationName(query, 5)
 			results.complete(searchResults.map {
 				val name = if (!it.getAddressLine(0).startsWith(it.featureName)) { it.featureName } else { "" }
+				val placeLocation = LatLong(it.latitude, it.longitude)
 				MapResult(it.url ?: "", name, it.addressLines().joinToString(", "),
-				LatLong(it.latitude, it.longitude))
+				placeLocation, currentLocation?.distanceFrom(placeLocation)?.toFloat())
 			})
 		} catch (e: IOException) {
 			results.complete(emptyList())
