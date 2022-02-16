ba457cd5c90f7bec0cbecd837c35737dc37248cb
Walter Huf <hufman@gmail.com>
2022-02-16 08:51:58 -0800

    More correctly updates Calendar phoneui list adapters

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/CalendarCalendarsFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/CalendarCalendarsFragment.kt
@@ -6,16 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.hufman.androidautoidrive.R
+import me.hufman.androidautoidrive.calendar.PhoneCalendar
 import me.hufman.androidautoidrive.phoneui.NestedListView
 import me.hufman.androidautoidrive.phoneui.adapters.DataBoundArrayAdapter
 import me.hufman.androidautoidrive.phoneui.viewmodels.CalendarEventsModel
-import me.hufman.androidautoidrive.phoneui.viewmodels.viewModels
+import me.hufman.androidautoidrive.phoneui.viewmodels.activityViewModels
 
 class CalendarCalendarsFragment: Fragment() {
-	val calendarEventsModel by viewModels<CalendarEventsModel> { CalendarEventsModel.Factory(requireContext().applicationContext) }
+	val calendarEventsModel by activityViewModels<CalendarEventsModel> { CalendarEventsModel.Factory(requireContext().applicationContext) }
+	val displayedCalendars = ArrayList<PhoneCalendar>()
 	val calendarsAdapter by lazy {
-		DataBoundArrayAdapter(requireContext(), R.layout.calendar_listitem, calendarEventsModel.calendars, null)
+		DataBoundArrayAdapter(requireContext(), R.layout.calendar_listitem, displayedCalendars, null)
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -35,8 +41,16 @@ class CalendarCalendarsFragment: Fragment() {
 	}
 
 	fun redraw() {
-		// update list of calendars
-		calendarEventsModel.update()
-		calendarsAdapter.notifyDataSetChanged()
+		// update list of upcoming events
+		viewLifecycleOwner.lifecycleScope.launch {
+			// calendar events are supposed to only be queried from a background thread
+			withContext(Dispatchers.IO) {
+				calendarEventsModel.update()
+			}
+			// but the ListView needs to be updated on the UI thread
+			displayedCalendars.clear()
+			displayedCalendars.addAll(calendarEventsModel.calendars)
+			calendarsAdapter.notifyDataSetChanged()
+		}
 	}
 }
