94b152ba9db560c65d881fb826e7ee2e8ef3958e
Walter Huf <hufman@gmail.com>
2022-02-25 10:50:27 -0800

    Shows a BT Maps Performance warning

--- app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MapsPageFragment.kt
+++ app/src/main/java/me/hufman/androidautoidrive/phoneui/fragments/MapsPageFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import me.hufman.androidautoidrive.databinding.MapPageSettingsBinding
+import me.hufman.androidautoidrive.phoneui.viewmodels.ConnectionStatusModel
 import me.hufman.androidautoidrive.phoneui.viewmodels.MapPageModel
+import me.hufman.androidautoidrive.phoneui.viewmodels.activityViewModels
 import me.hufman.androidautoidrive.phoneui.viewmodels.viewModels
 
 class MapsPageFragment: Fragment() {
-	val mapPageModel by viewModels<MapPageModel> { MapPageModel.Factory(requireContext().applicationContext) }
+	private val mapPageModel by viewModels<MapPageModel> { MapPageModel.Factory(requireContext().applicationContext) }
+	private val connectionViewModel by activityViewModels<ConnectionStatusModel> { ConnectionStatusModel.Factory(requireContext().applicationContext) }
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 		val binding = MapPageSettingsBinding.inflate(inflater, container, false)
 		binding.lifecycleOwner = viewLifecycleOwner
+		binding.connectionmodel = connectionViewModel
 		binding.viewmodel = mapPageModel
 		return binding.root
 	}
