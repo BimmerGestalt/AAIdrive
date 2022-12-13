2a693abdedf3032983ad48647f6a36b41eac3495
Walter Huf <hufman@gmail.com>
2022-12-12 18:14:23 -0800

    Hardcodes a browser root for Qobuz
    Obtained from a quick decompilation of Qobuz, hard to test without an account
    Should fix #644

--- app/src/main/java/me/hufman/androidautoidrive/music/MusicBrowser.kt
+++ app/src/main/java/me/hufman/androidautoidrive/music/MusicBrowser.kt
@@ -104,6 +104,7 @@ class MusicBrowser(val handler: Handler, val mediaBrowser: MediaBrowserCompat, v
 			"com.tbig.playerprotrial" -> "__ROOT__"    // PlayerPro Free
 			"com.tbig.playerpro" -> "__ROOT__"    // PlayerPro
 			"com.amazon.mp3" -> "BROWSER_ROOT"      // Amazon Music, except it sends a null root first
+			"com.qobuz.music" -> "QOBUZ_MEDIA_ROOT"     // Qobuz
 			else -> return when(musicAppInfo.className) {   // some apps have a shared service library
 				"com.itmwpb.vanilla.radioapp.player.MusicService" -> "/"    // OneCMS (HOT97 Official)
 				"com.example.android.uamp.media.MusicService" -> "/"        // UAMP Example player (Radio Bob)
