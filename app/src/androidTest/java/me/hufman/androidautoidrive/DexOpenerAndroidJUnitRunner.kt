0e969f61ad5fa934b44279bd502f348f87a5258e
Walter Huf <hufman@gmail.com>
2024-06-21 21:15:28 -0700

    Use a different mock for AndroidTests

--- app/src/androidTest/java/me/hufman/androidautoidrive/DexOpenerAndroidJUnitRunner.kt
+++ app/src/androidTest/java/me/hufman/androidautoidrive/DexOpenerAndroidJUnitRunner.kt
@@ -2,6 +2,7 @@ package me.hufman.androidautoidrive
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
 import androidx.test.runner.AndroidJUnitRunner
 import com.github.tmurakami.dexopener.DexOpener
 
@@ -9,7 +10,9 @@ import com.github.tmurakami.dexopener.DexOpener
 class DexOpenerAndroidJUnitRunner: AndroidJUnitRunner() {
 	@Throws(ClassNotFoundException::class, IllegalAccessException::class, InstantiationException::class)
 	override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application? {
-		DexOpener.install(this) // Call me first!
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+			DexOpener.install(this) // Call me first!
+		}
 		return super.newApplication(cl, className, context)
 	}
 }
