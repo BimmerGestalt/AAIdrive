c63c9e6b8f0e9372597c31947b71a3738f62656d
Walter Huf <hufman@gmail.com>
2024-07-24 20:42:42 -0700

    Add error handling around notification sounds

--- app/src/main/java/me/hufman/androidautoidrive/notifications/AudioPlayer.kt
+++ app/src/main/java/me/hufman/androidautoidrive/notifications/AudioPlayer.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import android.os.SystemClock
+import android.util.Log
+import me.hufman.androidautoidrive.carapp.notifications.TAG
+import java.lang.Exception
 
 class AudioPlayer(val context: Context) {
 	companion object {
@@ -17,14 +21,21 @@ class AudioPlayer(val context: Context) {
 	fun playRingtone(uri: Uri?): Boolean {
 		if (timeSinceLastPlayed < PLAYBACK_DEBOUNCE) return false
 		uri ?: return false
-		val ringtone = RingtoneManager.getRingtone(context, uri) ?: return false
-		ringtone.isLooping = false
-		ringtone.audioAttributes = AudioAttributes.Builder()
+		try {
+			val ringtone = RingtoneManager.getRingtone(context, uri) ?: return false
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+				ringtone.isLooping = false
+			}
+			ringtone.audioAttributes = AudioAttributes.Builder()
 				.setUsage(AudioAttributes.USAGE_MEDIA)
 				.setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
 				.build()
-		ringtone.play()
-		timeLastPlayed = SystemClock.elapsedRealtime()
+			ringtone.play()
+			timeLastPlayed = SystemClock.elapsedRealtime()
+		} catch (e: Exception) {
+			Log.e(TAG, "Error while playing notification sound", e)
+			return false
+		}
 		return true
 	}
 }
